2d0a6c32947a306bbe0b68b11a4d5b89a6398e48
zulfadlyzamry <zulfadlyzamry@gmail.com>
2026-08-26 16:59:01 +0800

    Update InteractionFlow.tsx

--- components/InteractionFlow.tsx
+++ components/InteractionFlow.tsx
@@ -340,7 +340,7 @@ const LoveMeterStep = ({ onComplete }: { onComplete: () => void }) => {
 
 // --- Step 4: Typewriter ---
 const TypewriterStep = ({ onComplete }: { onComplete: () => void }) => {
-    const text = "Happy Birthday!!!!";
+    const text = "Happy Birthday Sayang!!!!";
     const [displayedText, setDisplayedText] = useState("");
     const [isDeleting, setIsDeleting] = useState(false);
 
